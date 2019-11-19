547575360cb960bee9c961ad55e13de61e32dbc3
Justin Buchanan <buchanan@csgsolutions.com>
2019-11-19 15:50:38 -0600

    change test file ext

--- build.ps1
+++ build.ps1
@@ -47,7 +47,7 @@ try {
 		foreach ($test_proj in $TestProjects) {
 			Write-Host "Testing $test_proj"			
 			#Note: The --logger parameter is for specifically for mstest to make it output test results
-			dotnet test $test_proj --no-build --configuration $Configuration --logger "trx;logfilename=TEST-$(get-date -format yyyyMMddHHmmss).xml"
+			dotnet test $test_proj --no-build --configuration $Configuration --logger "trx;logfilename=TEST-$(get-date -format yyyyMMddHHmmss).trx"
 			if ($LASTEXITCODE -ne 0) {
 				throw "Test failed with code $LASTEXITCODE"
 			}
